52882b410436ba7f3d7857d4df7e855ee8197b52
CarstenLorenzSHE <cl@systemhaus-erdmann.de>
2026-03-06 10:03:28 +0100

    - added gitignore - removed parts from git cache

--- vendor/neuron-core/neuron-ai/src/Exceptions/ToolMaxTriesException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace NeuronAI\Exceptions;
-
-class ToolMaxTriesException extends ToolException
-{
-}
--- vendor/neuron-core/neuron-ai/src/Workflow/StartEvent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace NeuronAI\Workflow;
-
-class StartEvent implements Event
-{
-}
